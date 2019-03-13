98808a4ab0d6b45ddfb5f86cb5e41588e3a6c1b3
nefenit <nefenit@gmail.com>
2019-03-13 21:49:50 +0100

    Update bubble.c

--- bubble.c
+++ bubble.c
@@ -12,7 +12,7 @@ void swap(int *a, int *b) {
 	*b = *a;
 	*a =  t;
 }
-
+/* always worst case of O(n^2) */
 void bubblesort(int arr[], size_t n) {
 	int i, j;
 	
@@ -20,4 +20,23 @@ void bubblesort(int arr[], size_t n) {
 	for(j = 0; j < n - 1; j++)
 	if(arr[j] > arr[j + 1])
 	swap(&arr[j], &arr[j + 1]);
+	/*for(i = 0; i < n; i++)
+	for(j = n - 1; j > 0; j--)
+	if(a[j] < a[j - 1])
+	swap(a[j], a[j - 1]);*/
+}
+
+/* last element of array is always sorted after each outer loop iteration
+   this version capitalizes on this property */
+void bubblesort2(int arr[], size_t n) {
+	int i, j;
+	
+	for(i = 0; i < n; i++)
+	for(j = n - 1; j > i; j--)
+	if(a[j] < a[j - 1])
+	swap(a[j], a[j - 1]);
+	/*for(i = 0; i < n; i++)
+	for(j = 0; j < n - 1 - i; j++)
+	if(arr[j] > arr[j + 1])
+	swap(&arr[j], &arr[j + 1]);*/
 }
