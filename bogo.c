9f852adcbcedaeeebad63fd9d09d5b5824d2b75a
nefenit <nefenit@gmail.com>
2019-03-13 19:58:47 +0100

    Create bogo.c

---
+++ bogo.c
@@ -0,0 +1,34 @@
+/* bogo.c - bogo sort implementation
+ * (c) Copyright 2019 Bartosz Mierzynski
+ * Written in C99 (if you remove bool then C89)
+ */
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+void swap(int *x, int *y) {
+	int t = *x;
+	*x = *y;
+	*y = t;
+}
+
+void is_sorted(int arr[], size_t n) {
+	while(--n >= 1)
+		if(arr[n] < arr[n-1])
+			return false;
+	return true;
+}
+
+void shuffle(int arr[], size_t n) {
+	int i, r;
+	
+	for(i = 0; i < n; i++) {
+		r = rand() % n;
+		swap(&arr[i], &arr[r]);
+	}
+}
+
+void bogosort(int arr[], size_t n) {
+	while(!is_sorted(arr, n))
+		shuffle(arr, n);
+}
