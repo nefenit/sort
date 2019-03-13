47d590ef2ee5ee3184392647bcd587bcb9dd08a7
nefenit <nefenit@gmail.com>
2019-03-13 23:30:23 +0100

    Update bozo.c

--- bozo.c
+++ bozo.c
@@ -1,3 +1,19 @@
 /* bozo.c - randomly swaps elements until they are in order
  * (c) Copyright 2019 Bartosz Mierzynski
  */
+
+#include <stdlib.h>
+
+#include "sort.h"
+
+void bozosort(int arr[], size_t n) {
+	int i, j;
+	
+	srand(time(NULL));
+
+	while (!is_sorted(arr, n)) {
+		i = rand() % n;
+		j = rand() % n;
+		swap(&arr[i], &arr[j]);
+	}
+}
