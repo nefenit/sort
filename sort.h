c0e3f9f373a5e7977f0670c1220e7b61701552cc
nefenit <nefenit@gmail.com>
2019-03-13 22:29:42 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -4,7 +4,22 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+/* Sorts */
+void bogosort(int[], size_t);
+void bubblesort(int[], size_t);
+void bubblesort2(int[], size_t);
+void cocktail(int[], size_t);
+void oddevensort(int[], size_t);
+void quicksort_recursive_lomuto(int[], int, int);
+void quicksort_recursive_hoare(int[], int, int);
+void selectionsort(int[], size_t);
 
+/* Utilities */
+int partition_lomuto(int[], int, int);
+int partition_hoare(int[], int, int);
+bool is_sorted(int[], size_t);
+void shuffle(int[], size_t);
+void swap(int*, int*);
 
 
 #endif /* __SORT_H__ */
