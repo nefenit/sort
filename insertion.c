547296ae1f33ed7a92d79ca3442a3abf0b2d9ba4
nefenit <nefenit@gmail.com>
2019-03-14 20:24:42 +0100

    Create insertion.c

---
+++ insertion.c
@@ -0,0 +1,14 @@
+/* insertion.c - insertion sort implementation
+ * (c) Copyright 2019 Bartosz Mierzynski
+ */
+
+void insertionsort(int arr[], size_t n) {
+	int t, i, j;
+	
+	for(i = 1; i < n; ++i) {
+		t = arr[i];
+		for(j = i - 1; j >= 0 && arr[j] > t; --j)
+			arr[j + 1] = arr[j];
+		arr[j + 1] = t;
+	}
+}
