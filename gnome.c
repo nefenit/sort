815126dd38c2a86e56d0811b4887f7573b7e0610
nefenit <nefenit@gmail.com>
2019-03-14 19:28:14 +0100

    Create gnome.c

---
+++ gnome.c
@@ -0,0 +1,35 @@
+/* gnome.c - gnome sort implementation
+ * (c) Copyright 2019 Bartosz Mierzynski
+ */
+
+#include "sort.h"
+
+/* both of those are unoptimized versions */
+
+void gnomesort(int arr[], size_t n) {
+	int i = 0;
+	
+	while(i < n) {
+		if(i == 0 || arr[i] >= arr[i - 1]) {
+			++i;
+		} else {
+			swap(&arr[i], &arr[i - 1]);
+			--i;
+		}
+	}
+}
+
+
+void gnomesort1(int arr[], size_t n) {
+	int i = 1;
+	
+	while(i < n) {
+		if(a[i - 1] <= a[i]) {
+			++i;
+		} else {
+			swap(&a[i - 1], &a[i]);
+			--i;
+			if(i == 0)
+				i = 1;
+	}
+}
