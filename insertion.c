d242b74ce9ee32a65146150db6a154ad9a163a8f
nefenit <nefenit@gmail.com>
2019-03-14 20:47:31 +0100

    Update insertion.c

--- insertion.c
+++ insertion.c
@@ -5,12 +5,9 @@
 #include "sort.h"
 
 void insertionsort(int arr[], size_t n) {
-	int t, i, j;
+	int i, j;
 	
 	for(i = 1; i < n; ++i) {
-		t = arr[i];
-		for(j = i - 1; j >= 0 && arr[j] > t; --j)
-			arr[j + 1] = arr[j];
-		arr[j + 1] = t;
-	}
+	for(j = i; j > 0 && arr[j] < arr[j - 1]; --j)
+	swap(&arr[j], &arr[j - 1]);
 }
